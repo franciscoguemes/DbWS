7db502876bbafd3db451f9589abcc6ff7ab58156
Francisco Güemes <francisco.gueemes@uniscon.net>
2020-12-27 00:47:39 +0100

    Created the DbWS.sh script.

---
+++ DbWS.sh
@@ -0,0 +1,5 @@
+DbWS_DIRECTORY=/home/francisco/git/Francisco/github/DbWS
+CONFIG_DIRECTORY=/home/francisco/.config/DbWS
+
+
+$DbWS_DIRECTORY/DbWS.py --config=$CONFIG_DIRECTORY/DbWS.conf --logging=$CONFIG_DIRECTORY/logging.txt
