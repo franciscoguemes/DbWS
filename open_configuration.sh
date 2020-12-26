c3ef226b11297c5065982e5c430a7762ad58ce86
Francisco Güemes <francisco.gueemes@uniscon.net>
2020-12-26 21:22:16 +0100

    Update open_configuration script.

--- open_configuration.sh
+++ open_configuration.sh
@@ -1,3 +1,3 @@
 CONFIG_FOLDER=/home/francisco/.config/DbWS
 
-gedit $CONFIG_FOLDER/contexts_example.json  $CONFIG_FOLDER/contexts.json  $CONFIG_FOLDER/DbWS.conf &
+gedit $CONFIG_FOLDER/contexts_example.json  $CONFIG_FOLDER/contexts.json  $CONFIG_FOLDER/DbWS.conf $CONFIG_FOLDER/logging.conf&
