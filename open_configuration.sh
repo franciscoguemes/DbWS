e1bd6c4e3df4ffa13ca9d2c8175b6e5a9bf7b92e
Francisco Güemes <francisco.gueemes@uniscon.net>
2020-12-28 20:07:13 +0100

    Updated open_configuration.sh

--- open_configuration.sh
+++ open_configuration.sh
@@ -1,3 +1,3 @@
 CONFIG_DIRECTORY=/home/francisco/.config/DbWS
 
-gedit $CONFIG_DIRECTORY/contexts_example.json  $CONFIG_DIRECTORY/contexts.json  $CONFIG_DIRECTORY/DbWS.conf $CONFIG_DIRECTORY/logging.conf &
+gedit $CONFIG_DIRECTORY/contexts_example.json $CONFIG_DIRECTORY/contexts_test.json  $CONFIG_DIRECTORY/contexts.json  $CONFIG_DIRECTORY/DbWS.conf $CONFIG_DIRECTORY/logging.conf &
