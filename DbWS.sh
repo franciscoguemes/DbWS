46390f928b2cb81b96193c036ab707646cc41ac1
Francisco Güemes <francisco.gueemes@uniscon.net>
2020-12-28 19:01:21 +0100

    Updated script to execute the application.

--- DbWS.sh
+++ DbWS.sh
@@ -2,4 +2,4 @@ DbWS_DIRECTORY=/home/francisco/git/Francisco/github/DbWS
 CONFIG_DIRECTORY=/home/francisco/.config/DbWS
 
 
-$DbWS_DIRECTORY/DbWS.py --config=$CONFIG_DIRECTORY/DbWS.conf --logging=$CONFIG_DIRECTORY/logging.txt
+$DbWS_DIRECTORY/DbWS.py --config=$CONFIG_DIRECTORY/DbWS.conf --logging=$CONFIG_DIRECTORY/logging.conf
