8b2759b861bacc478a22a85f15721f1b581134c2
Francisco Güemes <francisco.gueemes@uniscon.net>
2020-12-29 12:24:54 +0100

    Add shebang to bash scripts.

--- DbWS.sh
+++ DbWS.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 DbWS_DIRECTORY=/home/francisco/git/Francisco/github/DbWS
 CONFIG_DIRECTORY=/home/francisco/.config/DbWS
 
-
-$DbWS_DIRECTORY/DbWS.py --config=$CONFIG_DIRECTORY/DbWS.conf --logging=$CONFIG_DIRECTORY/logging.conf
+cd $DbWS_DIRECTORY
+./DbWS.py --config=$CONFIG_DIRECTORY/DbWS.conf --logging=$CONFIG_DIRECTORY/logging.conf
--- open_configuration.sh
+++ open_configuration.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 CONFIG_DIRECTORY=/home/francisco/.config/DbWS
 
 gedit $CONFIG_DIRECTORY/contexts_example.json $CONFIG_DIRECTORY/contexts_test.json  $CONFIG_DIRECTORY/contexts.json  $CONFIG_DIRECTORY/DbWS.conf $CONFIG_DIRECTORY/logging.conf &
