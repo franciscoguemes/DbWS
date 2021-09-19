ab0d3eb378226396c9b5a3e14b28bffefa87fbf3
franciscoguemes <francisco.jose.guemes.sevilla@gmail.com>
2021-09-19 22:41:25 +0200

    Script now works independently of the current working directory

--- DbWS.sh
+++ DbWS.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-DbWS_DIRECTORY=/home/francisco/git/Francisco/github/DbWS
+DbWS_DIRECTORY=`dirname $0`
+DbWS_APP=$DbWS_DIRECTORY/DbWS.py
+DbWS_VENV=$DbWS_DIRECTORY/venv
 CONFIG_DIRECTORY=/home/francisco/.config/DbWS
 
-cd $DbWS_DIRECTORY
-./DbWS.py --config=$CONFIG_DIRECTORY/DbWS.conf --logging=$CONFIG_DIRECTORY/logging.conf
+# Setup the virtual environment as default python3 environment
+source $DbWS_VENV/bin/activate
+
+#****************************************************************************************************
+# Debug
+#****************************************************************************************************
+# Prints the executables that are being used (You may have multiple installations or virtual environments)
+# which pip
+# which python3
+
+# Start the application
+$DbWS_APP --config=$CONFIG_DIRECTORY/DbWS.conf --logging=$CONFIG_DIRECTORY/logging.conf
